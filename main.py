ca847052f0f935fd7edae4809aa87e97812d267f
mahmud20050 <olamasovmahmud@gmail.com>
2023-01-30 20:50:51 +0500

    pyton

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'alalalalallalaa {update.effective_user.first_name}')
+    await update.message.reply_html(f'alalalalallalaa <b>{update.effective_user.first_name}</b> \n \n<b>fdffgdgfgfgh</b> ')
 
 
 app = ApplicationBuilder().token("5903874788:AAE2e6E_55wN2cuLhX7AojBihSTmPUV8ZH0").build()
