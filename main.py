e06437f29a70ff104aca97aaa7afe7cbf483a3d8
mahmud20050 <olamasovmahmud@gmail.com>
2023-03-02 23:11:05 +0500

    python

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from telegram import Update
+from telegram import Updat
+from telegram import InlineKeyboardButton, InlinekeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_html(f'alalalalallalaa <b>{update.effective_user.first_name}</b> \n \n<b>fdffgdgfgfgh</b> ')
+    buttons = [
+        [
+            InlineKeyboardButton('Toshkent', collback_data='region_1'),
+            InlineKeyboardButton('Toshkent', collback_data='region_2')
+        ],
+    ]
+    await update.message.reply_html(
+        '😁ASALOMU AELKUM😁 <b> \n{update.effective_user.first_name}</b> \n \n<b>fdffgdgfgfgh</b>', replay_markup=InlinekeyboardMarkup(buttons))
 
 
 app = ApplicationBuilder().token("5903874788:AAE2e6E_55wN2cuLhX7AojBihSTmPUV8ZH0").build()
